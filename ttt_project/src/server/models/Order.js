e1bb579ca32152ad9006463b81c123a8d0084204
jakobsym <jakob.symchych@my.utsa.edu>
2023-10-29 13:10:25 -0500

    created an Order model to represent all orders placed, so it can be filtered easily.

---
+++ ttt_project/src/server/models/Order.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    products: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    orderDate: {
+        type: Date,
+        default: Date.now(),
+    },
+    orderStatus: {
+        type: String,
+        enum: ['Pending', 'Shipped', 'Delivered'],
+        default: 'Pending'
+    }
+});
+
+export default mongoose.model('Order', orderSchema);
